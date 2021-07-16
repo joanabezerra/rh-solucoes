8e2447f573135eb0c49b6e722777d3aecd8d59d1
Joana Moraes Marques Bezerra <joana.marques@math.marketing>
2021-07-16 10:45:55 -0300

    melhorias v2

--- js/funcionalidades.js
+++ js/funcionalidades.js
@@ -3,3 +3,22 @@
 const go = (elem) => {
     window.scroll({top: document.querySelector(elem).offsetTop, left: 0, behavior: 'smooth'});
 }
+
+// função para header fixo após scrool do usuário
+
+//chamando função do menu fixo
+window.onscroll = function(){menuFixo()};
+
+// pegando o elemento menu da página
+var menu = document.getElementById("menu");
+
+// offsetTop está pegando a posição exata do menu na página em relação ao topo
+var scrollPosition = menu.offsetTop;
+
+function menuFixo() {
+    if(window.pageYOffset > scrollPosition) {
+        menu.classList.add("scrollPosition");
+    } else {
+        menu.classList.remove("scrollPosition");
+    }
+}
